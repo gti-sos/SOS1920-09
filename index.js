1f2cddce72783622638a15d6e016fc80fe182330
pacomc <pacomc99@gmail.com>
2020-03-10 10:29:16 +0000

    add port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 var app = express();
 
+var port = process.env.PORT || 80; // Port is the default port and if it is not defined, it is 80
+
 app.use ("/", express.static("./public"));
 
-app.listen(80, () => {
+app.listen(port, () => {
 	console.log("Server ready!!");
 });
 
