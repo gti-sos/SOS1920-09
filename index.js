1ac5ed6c5b6be93bc7a2b76eea7db294ab6bebdf
pacomc <pacomc99@gmail.com>
2020-03-23 08:37:09 +0000

    initial api v3

--- index.js
+++ index.js
@@ -21,12 +21,20 @@ var plugInVehiclesStats = [
 
 var renewableSourcesStats = [
 	{ 
-		name: "peter",
-		phone: 123456	
+		"year": "Spain",
+		"country": 2016,
+		"percentage-re-total": 38.1,
+		"percentage-hydropower-total": 14.5,
+		"percentage-wind-power-total": 17.8
+		
 	},
 	{ 
-		name: "pablo",
-		phone: 789456	
+		"year": "France",
+		"country": 2016,
+		"percentage-re-total": 17.5,
+		"percentage-hydropower-total": 11.7,
+		"percentage-wind-power-total": 3.8
+		
 	}
 ];
 
@@ -71,9 +79,6 @@ const BASE_API_URL =  "/api/v1";
 
 
 // RESOURCE oilCoalNuclearEnergyConsumptionStats
-
-
-
 
 
 
